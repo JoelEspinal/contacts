9d973131528a0853125202de64d5ba813aac1995
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-01-23 03:04:21 -0400

    ✨ fetch contact to api

---
+++ app/src/main/java/com/joelespinal/contacts/remote/api/ContactApi.kt
@@ -0,0 +1,14 @@
+package com.joelespinal.contacts.remote.api
+
+import com.joelespinal.contacts.remote.response.ContactsResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface ContactApi {
+
+    @Headers("Content-Type: application/json")
+    @GET("/api")
+    suspend fun fetchUsers(@Query("results") limit: Int): Response<ContactsResponse>
+}
