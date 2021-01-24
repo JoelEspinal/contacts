cae2714da7f6b4b0936da7727e550c22d2577e5b
JoelEspinal <j.espinalcarrasco@gmail.com>
2021-01-24 14:34:03 -0400

    ✨ Add paging capability

--- app/src/main/java/com/joelespinal/contacts/ui/adapters/ContactAdapter.kt
+++ app/src/main/java/com/joelespinal/contacts/ui/adapters/ContactAdapter.kt
@@ -1,25 +1,16 @@
 package com.joelespinal.contacts.ui.adapters
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.joelespinal.contacts.databinding.ContactItemBinding
 import com.joelespinal.contacts.models.Contact
 import com.squareup.picasso.Picasso
 
-class ContactAdapter(private var contacts: MutableList<Contact>) :
-    RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
 
-    private lateinit var recyclerView: RecyclerView
-
-    inner class ContactViewHolder(var binding: ContactItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(contact: Contact) {
-            binding.name.text = "${contact.first}"
-        }
-    }
-
+class ContactAdapter() : PagedListAdapter<Contact, ContactViewHolder>(DIFF_CALLBACK) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val itemBinding = ContactItemBinding.inflate(layoutInflater, parent, false)
@@ -27,29 +18,40 @@ class ContactAdapter(private var contacts: MutableList<Contact>) :
     }
 
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
-        val contact = contacts[position]
+        val contact: Contact? = getItem(position)
 
-        holder.binding.name.text = "${contact.first}"
-
-
-        Picasso.get()
-            .load(contact.thumbnail)
-            .resize(200, 200)
-            .centerCrop()
+        if (contact != null) {
+            holder.binding.name.text = "${contact.first}"
+            Picasso.get()
+                .load(contact.thumbnail)
+                .resize(200, 200)
+                .centerCrop()
 //            .placeholder(R.drawable.user_placeholder)
 //            .error(R.drawable.user_placeholder_error)
-            .into(holder.binding.picture);
+                .into(holder.binding.picture)
+        }
+
+
     }
 
-    override fun getItemCount(): Int = contacts.size
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Contact>() {
+            // The ID property identifies when items are the same.
+            override fun areItemsTheSame(oldItem: Contact, newItem: Contact) =
+                oldItem.phone == newItem.phone
 
-    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
-        super.onAttachedToRecyclerView(recyclerView)
-        this.recyclerView = recyclerView
+            // If you use the "==" operator, make sure that the object implements
+            // .equals(). Alternatively, write custom data comparison logic here.
+            override fun areContentsTheSame(
+                oldItem: Contact, newItem: Contact
+            ) = oldItem == newItem
+        }
     }
+}
 
-    fun updateContacts(contacts: MutableList<Contact>) {
-        this.contacts = contacts
-        notifyDataSetChanged()
+class ContactViewHolder(var binding: ContactItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(contact: Contact) {
+        binding.name.text = "${contact.first}"
     }
 }
